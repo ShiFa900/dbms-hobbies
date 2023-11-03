9555bd85b753d2f6a4ea448d28f9e28f1434512a
Shifa <luh.shifa@gmail.com>
2023-11-03 15:33:14 +0800

    Friday, Nov 03 2023

---
+++ hobbiesdb-dump.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: hobbies
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP SCHEMA IF EXISTS hobbiesdb;
+CREATE SCHEMA hobbiesdb;
+USE hobbiesdb;
+
+--
+-- Table structure for table `hobbies`
+--
+
+DROP TABLE IF EXISTS `hobbies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hobbies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hobbies`
+--
+
+LOCK TABLES `hobbies` WRITE;
+/*!40000 ALTER TABLE `hobbies` DISABLE KEYS */;
+INSERT INTO `hobbies` VALUES (1,'Nonton film'),(2,'Membaca'),(3,'Berenang'),(4,'Main game'),(5,'Melayangan');
+/*!40000 ALTER TABLE `hobbies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `person_hobbies`
+--
+
+DROP TABLE IF EXISTS `person_hobbies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `person_hobbies` (
+  `person_id` int DEFAULT NULL,
+  `hobby_id` int DEFAULT NULL,
+  KEY `person_hobbies_hobbies_id_fk` (`hobby_id`),
+  KEY `person_hobbies_persons_id_fk` (`person_id`),
+  CONSTRAINT `person_hobbies_hobbies_id_fk` FOREIGN KEY (`hobby_id`) REFERENCES `hobbies` (`id`),
+  CONSTRAINT `person_hobbies_persons_id_fk` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person_hobbies`
+--
+
+LOCK TABLES `person_hobbies` WRITE;
+/*!40000 ALTER TABLE `person_hobbies` DISABLE KEYS */;
+INSERT INTO `person_hobbies` VALUES (1,1),(1,2),(2,3),(3,4),(4,4);
+/*!40000 ALTER TABLE `person_hobbies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persons`
+--
+
+DROP TABLE IF EXISTS `persons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persons` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persons`
+--
+
+LOCK TABLES `persons` WRITE;
+/*!40000 ALTER TABLE `persons` DISABLE KEYS */;
+INSERT INTO `persons` VALUES (1,'Shifa'),(2,'Nilam'),(3,'Cahya'),(4,'Dipa'),(5,'Gemboel');
+/*!40000 ALTER TABLE `persons` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-02 19:15:24
---
+++ hobby-query.sql
@@ -0,0 +1,66 @@
+# SOAL 1
+SELECT p.id   AS 'Person ID',
+       p.name AS 'Name',
+       h.id   AS 'Hobby ID',
+       h.name AS Hobby
+
+
+FROM persons p
+
+         JOIN person_hobbies ph on p.id = ph.person_id
+         JOIN hobbies h on h.id = ph.hobby_id
+;
+
+# SOAL 2
+SELECT p.id   AS 'Person ID',
+       p.name AS 'Name',
+       h.id   AS 'Hobby ID',
+       h.name AS Hobby
+
+
+FROM persons p
+
+# jika ID person tidak ditemukan di tabel person_Hobbies maka tampilkan null
+
+         LEFT JOIN person_hobbies ph on p.id = ph.person_id
+         LEFT JOIN hobbies h on h.id = ph.hobby_id
+;
+
+# SOAL 3
+SELECT p.id   AS 'Person ID',
+       p.name AS 'Name',
+       h.id   AS 'Hobby ID',
+       h.name AS Hobby
+
+
+FROM persons p
+
+# jika ID Hobbies tidak ditemukan di tabel person_Hobbies maka tampilkan null
+
+         RIGHT JOIN person_hobbies ph on p.id = ph.person_id
+         RIGHT JOIN hobbies h on h.id = ph.hobby_id
+;
+
+# SOAL 4
+SELECT p.id   AS 'Person ID',
+       p.name AS Name,
+       h.id   AS 'Hobby ID',
+       h.name AS Hobby
+
+FROM persons p
+         LEFT JOIN person_hobbies ph on p.id = ph.person_id
+         LEFT JOIN hobbies h on h.id = ph.hobby_id
+UNION ALL
+SELECT p.id   AS 'Person ID',
+       p.name AS Name,
+       h.id   AS 'Hobby ID',
+       h.name AS Hobby
+FROM persons p
+         RIGHT JOIN person_hobbies ph on p.id = ph.person_id
+         RIGHT JOIN hobbies h on h.id = ph.hobby_id
+WHERE p.id is null
+;
+
+
+
+
